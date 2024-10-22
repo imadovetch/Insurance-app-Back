e8e8235ef3a606b980cccd3fd8ba1c38113edcbb
imadovetch <ibouderoua63@gmail.com>
2024-10-22 12:16:54 +0200

    Stable envirement Spring Core and Spring Web included

---
+++ src/main/java/com/programming/techie/EmailClient.java
@@ -0,0 +1,18 @@
+package com.programming.techie;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+class EmailClient {
+    @Qualifier("basicSpellChecker")
+    @Autowired
+
+    private SpellChecker spellChecker;
+
+    void sendEmail(String emailMessage) {
+        spellChecker.checkSpelling(emailMessage);
+        // Logic to Send Email
+    }
+}
